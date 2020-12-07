53087a39ffc5a9d262bde16dd341701ab0bc74aa
Bryan Burns <bryanburns93@gmail.com>
2020-12-07 16:44:24 -0500

    add response code

--- week6d1/http-from-zero-to-server-starter/server.js
+++ week6d1/http-from-zero-to-server-starter/server.js
@@ -2,6 +2,10 @@ const http = require('http')
 
 http.createServer((req, res) => {
     //res.write('<h1>Hello World</h1>')
+    if (req.url === '/OK') {
+        console.log('Inbound \'OK\' request being processed...')
+    }
+    res.writeHead(200)
     res.end()
 }).listen(3000, ()=>{
     console.log('Welcome to the Server')
