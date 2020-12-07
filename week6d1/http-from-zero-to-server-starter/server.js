7b7e0efd971303514f8c794b5e235a9ba4cdbee8
Caleb <caleb-gilbert@live.com>
2020-12-07 17:34:58 -0500

    phase 3a complete

--- week6d1/http-from-zero-to-server-starter/server.js
+++ week6d1/http-from-zero-to-server-starter/server.js
@@ -1,52 +1,52 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-    //res.write('<h1>Hello World</h1>')
+    res.write('<h1>Hello World</h1>')
     switch(req.url){
         case '/OK': 
         console.log('INBOUND: /OK')
         res.writeHead(200)
         break;
-        case 'Bad-Request':
+        case '/Bad-Request':
             console.log('INBOUND: Bad-Request')
             res.writeHead(400)
         break;
-        case 'Created':
+        case '/Created':
             console.log('INBOUND: Created')
             res.writeHead(201)
         break;
-        case 'Forbidden':
+        case '/Forbidden':
             console.log('INBOUND: Forbidden')
             res.writeHead(403)
         break;
-        case 'Found':
+        case '/Found':
             console.log('INBOUND: Found')
             res.writeHead(302)
         break;      
-        case 'Gateway-Timeout':
+        case '/Gateway-Timeout':
             console.log('INBOUND: Gateway-Timeout')
             res.writeHead(504)  
         break;
-        case 'Internal-Server-Error':
+        case '/Internal-Server-Error':
             console.log('INBOUND: Internal-Server-Error')
             res.writeHead(500)
         break;
-        case 'Moved-Permanently':
+        case '/Moved-Permanently':
             console.log('INBOUND: Moved-Permanently')
             res.writeHead(301)
         break;
-        case 'Unauthorized':
+        case '/Unauthorized':
             console.log('INBOUND: Unauthorized')
             res.writeHead(401)
         break;
-        case 'Not-Found':
+        case '/Not-Found':
             console.log('INBOUND: Not-Found')
             res.writeHead(404)
         break;
     }
 
     res.end()
-}).listen(3000, ()=>{
+}).listen(8080, ()=>{
     console.log('Welcome to the Server')
 })
 
