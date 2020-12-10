3baa8c9bc7c5db39b636bfa579c4a3fb1e43570a
Caleb <caleb-gilbert@live.com>
2020-12-10 16:48:01 -0500

    1/2 through phase 0

--- week6d4/tdd-project/problems/reverse-string.js
+++ week6d4/tdd-project/problems/reverse-string.js
@@ -1,1 +1,6 @@
-hello
+
+const reverseString = function (str) {
+  return str.split('').reverse().join('')
+}
+
+module.exports = { reverseString }
--- week6d4/tdd-project/test/reverse-string-spec.js
+++ week6d4/tdd-project/test/reverse-string-spec.js
@@ -1,1 +1,38 @@
-hello
+const chai = require('chai');
+const spies = require('chai-spies');
+
+const expect = chai.expect;
+
+chai.use(spies);
+
+const reverseString = require('../problems/reverse-string')
+
+// console.log(reverseString())
+
+
+
+describe("reverse", () => {
+  it('should return string in reverse', () => {
+    const input = 'fun';
+    const expected = 'nuf';
+
+    const result = reverseString(input);
+
+    expect(result).to.equal(expected);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
