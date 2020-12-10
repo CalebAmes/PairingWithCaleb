356c3ce4f411b3d8b76ecc8640de4a217eaee736
Caleb <caleb-gilbert@live.com>
2020-12-10 17:38:10 -0500

    phase 0 completed

--- week6d4/tdd-project/test/reverse-string-spec.js
+++ week6d4/tdd-project/test/reverse-string-spec.js
@@ -10,18 +10,18 @@ const reverseString = require('../problems/reverse-string.js')
 
 describe("reverse", () => {
   it('should return string in reverse', () => {
-    const input = 5;
+    const input = 'fun';
     const expected = 'nuf';
 
     const result = reverseString(input);
 
     expect(result).to.equal(expected);
-
   });
 
-  it(typeof reverseString() !== "string") {
-    throw Error('Error')
-  }
+  it('should throw and error with a invalid arg', () => {
+    expect(() => reverseString()).to.throw(TypeError)
+  })
+
 
 
 
