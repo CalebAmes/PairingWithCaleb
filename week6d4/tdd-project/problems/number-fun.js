04e0a247fed23b27da79b996ca7f6e9d8b2d1595
Caleb <caleb-gilbert@live.com>
2020-12-10 18:55:57 -0500

    phase 2

--- week6d4/tdd-project/problems/number-fun.js
+++ week6d4/tdd-project/problems/number-fun.js
@@ -1,4 +1,5 @@
 const reciprocal = (num) => {
+    if(num < 1 || num > 1000000) throw TypeError('Error');
     return 1 / num;
 }
 
--- week6d4/tdd-project/test/number-fun-spec.js
+++ week6d4/tdd-project/test/number-fun-spec.js
@@ -17,18 +17,21 @@ describe('returnsThree', () => {
 
 describe('reciprocal', () => {
   it('return reciprocal', () => {
-    const value1 = reciprocal(8);
-    const result1 = 0.125;
     const value2 = reciprocal(10);
     const result2 = 0.1;
     const value3 = reciprocal(5);
     const result3 = 0.2;
-
-    assert.strictEqual(value1, result1)
+    
+    
     assert.strictEqual(value2, result2)
     assert.strictEqual(value3, result3)
   })
+  
+  it('return error', () => {
+    assert.throws(() => reciprocal(-1), TypeError, 'Error')
+    
 
+  })
 
 
 })
