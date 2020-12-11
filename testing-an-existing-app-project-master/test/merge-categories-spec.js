eb179998328fa79eb0bc9e7739942f5072797eb3
Caleb <caleb-gilbert@live.com>
2020-12-11 16:20:42 -0500

    added w6d5 project

---
+++ testing-an-existing-app-project-master/test/merge-categories-spec.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai');
+const { mergeCategories } = require('../merge-categories');
+
+describe("mergeCategories()", () => {
+  context("Using <li> tags", () => {
+    const template = `
+      <div>
+        <ul>
+          <!-- Content here -->
+        </ul>
+      </div>
+    `;
+
+    it("should return no <li>s for no categories", () => {
+    });
+
+    it("should return a single <li> for one category", () => {
+      expect.fail('please write this test');
+    });
+
+    it("should return an <li> for each category", () => {
+      expect.fail('please write this test');
+    });
+  });
+
+  context("using <option> tags", () => {
+    const template = `
+      <div>
+        <select>
+          <!-- Content here -->
+        </select>
+      </div>
+    `;
+
+    it("should return no <option>s for no categories", () => {
+      expect.fail('please write this test');
+    });
+
+    it("should return a single <option> for one category", () => {
+      expect.fail('please write this test');
+    });
+
+    it("should return an <option> for each category", () => {
+      expect.fail('please write this test');
+    });
+  });
+});
