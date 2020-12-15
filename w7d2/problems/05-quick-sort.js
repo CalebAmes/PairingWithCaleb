9674c48552da6dd9c857a1af0f4df1649a378e7c
Caleb <caleb-gilbert@live.com>
2020-12-15 18:19:19 -0500

    quick sort

--- w7d2/problems/05-quick-sort.js
+++ w7d2/problems/05-quick-sort.js
@@ -1,9 +1,48 @@
 // Implement Quick Sort
+/*
+Base Case:
+if array.length is <= 1 return array
 
+let pivot = array.shift()
+
+let left
+let right
+
+forloop
+  if el < pivot 
+    left.push el
+  if el > pivot
+    right.push el
+
+let leftSorted = quickSort(left)
+let rightSorted = quickSort(right)
+
+return leftSorted.concat(pivot).concat(rightSorted)
+
+
+
+*/
 function quickSort(array) {
-  // your code here
+  if (array.length <=1) return array;
+
+  let pivot = array.shift()
+  let left = [];
+  let right = [];
+
+  for(let i = 0; i < array.length; i++){
+    let el = array[i];
+    if(el < pivot) left.push(el);
+    if(el > pivot) right.push(el);
+  }
+
+  let leftSorted = quickSort(left);
+  let rightSorted = quickSort(right);
+
+  return leftSorted.concat(pivot).concat(rightSorted)
 }
 
+let test = [32, 12, 4, 1, 0, 53, 9, 5]
+console.log(quickSort(test));
 
 module.exports = {
   quickSort
