17b8622e4653023433e7edb12b0cd79a9986c458
James Aikens <jaikens19@hotmail.com>
2020-12-16 17:27:36 -0600

    finished firstAnagram

--- big-o-intro-project-master/02-anagrams.js
+++ big-o-intro-project-master/02-anagrams.js
@@ -1,8 +1,18 @@
+
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  if(str1.length !== str2.length) return false
+  for(let i = 0; i < str1.length; i++){
+
+    let index1 = str1.indexOf(str2[i])
+    let index2 = str2.indexOf(str1[i])
+
+    if(index1 === -1) return false
+    if(index2 === -1) return false
+  }
+  return true
 }
 
-
+console.log(firstAnagram('livee', 'elvis'))
 
 function secondAnagram(str1, str2) {
   // Code goes here ....
