988d8851e66fa09deb2595d1aa85855ef05198a7
B Salinas <b.salinas397@gmail.com>
2020-12-17 15:03:56 -0600

    push method on class Stack

--- data-structures-stack-starter-master/lib/stack.js
+++ data-structures-stack-starter-master/lib/stack.js
@@ -21,12 +21,42 @@
 // -----------
 
 class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 
 }
 
 class Stack {
+  constructor(length = 0) {
+    this.length = length;
+    this.store = new Array();
+    this.top = this.store[0];
+  }
+
+  push(val) { // should accept a single input parameter
+    let topEl = new Node(val); // should create a new node with the value provided as input
+    this.top = topEl; // should reassign both the top pointer when a new node is added to the top of an empty stack
+    this.store.push(topEl); 
+    // topEl = this.head;
+    const newTop = topEl.next
+    return ++this.length;
+  }
+
+  pop() {}
+
+  size() {}
+
+
 
 }
+
+const newArr = new Stack();
+newArr.push(9);
+newArr.push(1);
+newArr.push(3);
+console.log(newArr);
 
 exports.Node = Node;
 exports.Stack = Stack;
