2139aaaaf22b02b9ac97c9be56f490442eaf35ae
B Salinas <b.salinas397@gmail.com>
2020-12-17 18:15:33 -0600

    trouble w dequeue

--- data-structures-queue-starter-master/lib/queue.js
+++ data-structures-queue-starter-master/lib/queue.js
@@ -49,8 +49,37 @@ class Queue {
 	}
   
 	dequeue() {
-    
-		return --this.length;
+		let oldFront;
+		let newFront; // this.front.next
+		
+		--this.length
+		if (this.length <= 0) return 0;
+		if (this.length == 1) return null;
+		if (this.length == 2) {
+			let res = this.front;
+			this.front = null;
+			this.back = null;
+			return res.value;
+		}
+		// if (this.length == 3) {
+			// 	oldFront = this.front;
+			// 	newFront = oldFront.next;
+			// 	this.front = newFront;
+			// 	newFront.next = this.back;
+			// }
+			
+			oldFront = this.front;
+			newFront = oldFront.next;
+			this.front = newFront;
+			
+			if (this.length == 4) {
+				newFront.next = this.back;
+			}
+			
+			if (this.length == 3) {
+				this.front = this.back;
+			}
+		return oldFront.value;
 	}
 
 	size() {
@@ -60,12 +89,14 @@ class Queue {
 const potato = new Queue();
 
 potato.enqueue('fries');
-potato.enqueue('mashed potatos');
-potato.enqueue('tater tots');
-potato.enqueue('hash browns');
-potato.enqueue('gnocchi')
+// potato.enqueue('mashed potatos');
+// potato.enqueue('tater tots');
+// potato.enqueue('hash browns');
+// potato.enqueue('gnocchi')
 
-console.log(potato);
+// console.log(potato);
+console.log(potato.dequeue())
+// console.log(potato.dequeue())
 
 exports.Node = Node;
 exports.Queue = Queue;
