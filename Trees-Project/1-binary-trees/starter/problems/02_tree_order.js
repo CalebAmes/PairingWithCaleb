c23e4c5196e755f1877b73d2661a7c4a8f548bb6
Jamie Kichuk <jckichuk@gmail.com>
2020-12-28 16:16:29 -0600

    treeNode

--- Trees-Project/1-binary-trees/starter/problems/02_tree_order.js
+++ Trees-Project/1-binary-trees/starter/problems/02_tree_order.js
@@ -1,5 +1,10 @@
-function preOrderArray(root) {
+function preOrderArray(root, result = []) {
+  if (root === null) return
 
+  result.push(root.val)
+
+  preOrderArray(root.left, result)
+  preOrderArray(root.right, result)
 }
 
 function inOrderArray(root) {
