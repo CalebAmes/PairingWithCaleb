c745b9733319fb56326c8eb98de6414b7308293a
Caleb <caleb-gilbert@live.com>
2020-12-28 17:35:36 -0500

    working on dfs

--- Trees-Project/1-binary-trees/starter/problems/02_tree_order.js
+++ Trees-Project/1-binary-trees/starter/problems/02_tree_order.js
@@ -1,18 +1,37 @@
-function preOrderArray(root, result = []) {
-  if (root === null) return
+function preOrderArray(root) {
+  if (root === null) return [];
 
+  let result = [];
   result.push(root.val)
 
-  preOrderArray(root.left, result)
-  preOrderArray(root.right, result)
+  result = result.concat(preOrderArray(root.left));
+  
+  result = result.concat(preOrderArray(root.right));
+  return result;
 }
 
 function inOrderArray(root) {
+  if(root === null) return [];
 
+  let result = [];
+  // console.log(root.left, `\n`, root.val)
+  result = result.concat(inOrderArray(root.left));
+  result.push(root.val);
+  result = result.concat(inOrderArray(root.right));
+
+  return result;
 }
 
 function postOrderArray(root) {
+  if(root === null) return [];
 
+  let result = [];
+
+  result = result.concat(postOrderArray(root.left));
+  result = result.concat(postOrderArray(root.right));
+  result.push(root.val);
+
+  return result;
 }
 
 
--- Trees-Project/1-binary-trees/starter/problems/03_dfs.js
+++ Trees-Project/1-binary-trees/starter/problems/03_dfs.js
@@ -1,5 +1,13 @@
 function dfs(root){
+  if(root === null) return [];
 
+  let res = [];
+
+  res = res.concat(dfs(root.left));
+  res.push(root.val);
+  res = res.concat(dfs(root.right))
+
+  return res;
 }
 
 module.exports = { dfs };
