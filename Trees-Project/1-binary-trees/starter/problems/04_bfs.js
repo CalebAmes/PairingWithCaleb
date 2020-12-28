d8fb48510235b6d976bbebd6697dc1a844b48da5
Jamie Kichuk <jckichuk@gmail.com>
2020-12-28 17:09:54 -0600

    bfs

--- Trees-Project/1-binary-trees/starter/problems/04_bfs.js
+++ Trees-Project/1-binary-trees/starter/problems/04_bfs.js
@@ -1,5 +1,12 @@
-function bfs(root){
+function bfs(root) {
+  if (root === null) return []
 
+  let result = []
+  result.push(root.val)
+  result = result.concat(bfs(root.left))
+  result = result.concat(bfs(root.right))
+
+  return result
 }
 
 module.exports = { bfs };
