390e58c13eebbbddecaf1e522b81a5c8aa153c46
Jamie Kichuk <jckichuk@gmail.com>
2020-12-29 10:09:59 -0600

    update

--- Trees-Project/2-binary-search-tree/starter/problems/03-getHeight.js
+++ Trees-Project/2-binary-search-tree/starter/problems/03-getHeight.js
@@ -1,4 +1,13 @@
 function getHeight(root) {
+  if (root === null) return -1
+
+  let result = [];
+  result.push(root.val)
+
+  result = result.concat(getHeight(root.left));
+
+  result = result.concat(getHeight(root.right));
+  return result.length
 
 }
 
