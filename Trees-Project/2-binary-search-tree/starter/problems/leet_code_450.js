df56ab98302af3c085654f5cbbee0ecdf30c65b1
Jamie Kichuk <jckichuk@gmail.com>
2020-12-29 14:54:50 -0600

    delete node v1

--- Trees-Project/2-binary-search-tree/starter/problems/leet_code_450.js
+++ Trees-Project/2-binary-search-tree/starter/problems/leet_code_450.js
@@ -1,6 +1,28 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/delete-node-in-a-bst/
 
-function deleteNode(root, key) {
+// key is the target node we need to delete
+//  if key has no children (is leaf) -> then just delete it
+//  if key only has one child, then replace key with child
+//  if key has 2 children, go right one, then all the way to the left (until null)
+//    replace target node val with current node val
+//    call delete on current node
 
+
+
+function deleteNode(root, key) {
+  let surfer = root
+  while ((surfer.val !== key) && (surfer !== null)) {
+    if (key > surfer.val) surfer = surfer.right
+    else surfer = surfer.left
+
+    // key is not in BST
+    if (surfer === null) return false
+  }
+
+  if (surfer.right === null && surfer.left === null) {
+    surfer = null
+  }
+
+  return false
 }
