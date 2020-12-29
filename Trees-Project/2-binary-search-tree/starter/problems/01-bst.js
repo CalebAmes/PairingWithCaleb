f7bd3c39f930d8093a7e62728cfb96fbbcca7e3e
Jamie Kichuk <jckichuk@gmail.com>
2020-12-28 18:05:20 -0600

    bst

--- Trees-Project/2-binary-search-tree/starter/problems/01-bst.js
+++ Trees-Project/2-binary-search-tree/starter/problems/01-bst.js
@@ -7,10 +7,38 @@ class TreeNode {
 }
 
 class BST {
+  constructor() {
+    this.root = null
+  }
 
+  insert(val) {
+    let newNode = new TreeNode(val)
+
+    if (this.root === null) return this.root = newNode
+
+    let right = this.root.right
+    let left = this.root.left
+    while (this.root !== null) {
+      if (newNode > this.root.val) {
+        if (right === null) right = newNode
+        else right = this.root
+      }
+      else {
+        if (this.root.left === null) this.root.left = newNode
+        else this.root.left = this.root
+      }
+
+    }
+
+
+  }
+
+  searchRecur(val) {
+
+  }
 }
 
 module.exports = {
-    TreeNode, 
-    BST
+  TreeNode,
+  BST
 };
