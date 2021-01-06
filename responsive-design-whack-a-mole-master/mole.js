92e4de1dfe0b245f7759eb114e3bd1e0ff0c8c20
Caleb <caleb-gilbert@live.com>
2021-01-06 16:32:56 -0500

    random mole

--- responsive-design-whack-a-mole-master/mole.js
+++ responsive-design-whack-a-mole-master/mole.js
@@ -1,12 +1,21 @@
 // Write your JS here.
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
 
-//   setInterval(() => {
-//     const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//     for (let moleHead of moleHeads) {
-//       moleHead.classList.toggle('wgs__mole-head--hidden');
-//     }
-//   }, 1000);
+  // setInterval(() => {
+  //   const moleHeads = document.querySelectorAll('.wgs__mole-head');
+  //   for (let moleHead of moleHeads) {
+  //     moleHead.classList.toggle('wgs__mole-head--hidden');
+  //   }
+  // }, 1000);
 
-  
-// })
+  const popUpRandomMole = () => {
+    let moles = document.getElementsByClassName('wgs');
+    let random = Math.floor(Math.random() * 7);
+    moles[random].innerHTML = `<div class="wgs">
+    <img src = "mole-head.png" class="wgs__mole-head ">
+    <img src = "mole-hill.png" class="wgs__dirt-pile"> 
+  </div>`;
+  }
+  popUpRandomMole()
+// let random = Math.floor(Math.random()*7);
+})
