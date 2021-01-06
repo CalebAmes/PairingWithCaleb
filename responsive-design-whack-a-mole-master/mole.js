205fef61c29af90fc64dffb9ffdd03e3e94046d5
Caleb <caleb-gilbert@live.com>
2021-01-06 15:32:27 -0500

    sliding hogs

--- responsive-design-whack-a-mole-master/mole.js
+++ responsive-design-whack-a-mole-master/mole.js
@@ -1,1 +1,12 @@
 // Write your JS here.
+window.addEventListener('DOMContentLoaded', () => {
+
+  setInterval(() => {
+    const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    for (let moleHead of moleHeads) {
+      moleHead.classList.toggle('wgs__mole-head--hidden');
+    }
+  }, 1000);
+
+  
+})
