301471e53a5215910c2426ac0c72f8c5286a854a
Caleb <caleb-gilbert@live.com>
2021-01-07 16:53:20 -0500

    phase 4 in prog

--- ajax-project-starter-master/public/events.js
+++ ajax-project-starter-master/public/events.js
@@ -1,31 +1,49 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 	const imgSetter = async () => {
-        let loader = document.querySelector(".loader");
-        loader.innerText = "Loading...";
+		let loader = document.querySelector('.loader');
+		loader.innerText = 'Loading...';
 
-        try {
-            let catPic = document.querySelector('.cat-pic');
-            const image = await fetch('/kitten/image');
+		try {
+			let catPic = document.querySelector('.cat-pic');
+			const image = await fetch('/kitten/image');
 
-            if (!image.ok) {
-                throw image;
-            }
-            const json = await image.json();
+			if (!image.ok) throw image;
+			const json = await image.json();
 
-            catPic.src = json.src;
-            loader.innerHTML = "";
-
-        } catch (err) {
-            let errMessage = await err.json();
-            loader.innerText = "";
-            document.querySelector(".error").innerHTML = errMessage.message;
-        }
+			catPic.src = json.src;
+			loader.innerHTML = '';
+		} catch (err) {
+			handleError(err);
+			loader.innerText = '';
+		}
 	};
-    imgSetter();
 
-    document.getElementById('new-pic').addEventListener('click', (event) => {
+	document.getElementById('new-pic').addEventListener('click', (event) => {
+		imgSetter();
+	});
 
-        imgSetter();
+	imgSetter();
 
-    });
+	const handleError = async (err) => {
+		let errMessage = await err.json();
+		document.querySelector('.error').innerHTML = errMessage.message;
+    };
+    
+    //phase 4 Voting
+    let upvote = document.getElementById('upvote');
+    let downvote = document.getElementById('downvote');
+
+    const upvoteFunc = async () => {
+        try {
+            let fetched = await fetch('/kitten/upvote');
+
+            if(!fetched.ok) throw fetched;
+            // const json = await fetched.json();
+        } catch (err) {
+            handleError(err);
+        }
+    }
+    upvote.addEventListener('click', (event)  => {
+        upvoteFunc();
+    })
 });
