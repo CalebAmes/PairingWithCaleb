3b8d9bc9b3197651d865abdef2d0c5602ae91190
Ted Anton <tedjanton@gmail.com>
2021-01-07 15:22:04 -0600

    finished phase 2

--- ajax-project-starter-master/index.js
+++ ajax-project-starter-master/index.js
@@ -12,7 +12,7 @@ const port = 3000;
 // Controls the probability of an error being thrown. If ERROR_RATE is 0, then
 // an error will never be thrown. If ERROR_RATE is 100, then an error will
 // always be thrown:
-const ERROR_RATE = 0;
+const ERROR_RATE = 100;
 
 const getRandomInt = () => {
   // generates integer from 0 to 99:
--- ajax-project-starter-master/public/events.js
+++ ajax-project-starter-master/public/events.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 	const imgSetter = async () => {
-		let catPic = document.querySelector('.cat-pic');
-		const image = await fetch('/kitten/image');
-		const json = await image.json();
+        let loader = document.querySelector(".loader");
+        loader.innerText = "Loading...";
 
-		catPic.src = json.src;
+        try {
+            let catPic = document.querySelector('.cat-pic');
+            const image = await fetch('/kitten/image');
+
+            if (!image.ok) {
+                throw image;
+            }
+            const json = await image.json();
+
+            catPic.src = json.src;
+            loader.innerHTML = "";
+
+        } catch (err) {
+            let errMessage = await err.json();
+            loader.innerText = "";
+            document.querySelector(".error").innerHTML = errMessage.message;
+        }
 	};
     imgSetter();
-    document.getElementById('new-pic').addEventListener('click', imgSetter)
+
+    document.getElementById('new-pic').addEventListener('click', (event) => {
+
+        imgSetter();
+
+    });
 });
