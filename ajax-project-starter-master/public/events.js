99ec437656722e1f76f688e2b491b8550dd6aa81
Caleb <caleb-gilbert@live.com>
2021-01-07 15:41:06 -0500

    kitty images

--- ajax-project-starter-master/public/events.js
+++ ajax-project-starter-master/public/events.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded" , (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
+	const imgSetter = async () => {
+		let catPic = document.querySelector('.cat-pic');
+		const image = await fetch('/kitten/image');
+		const json = await image.json();
 
-    document.getElementsByClassName("cat-pic");
-
-    
-    const image = await fetch("/kitten/image")
-
-
-
-
-})
+		catPic.src = json.src;
+	};
+    imgSetter();
+    document.getElementById('new-pic').addEventListener('click', imgSetter)
+});
