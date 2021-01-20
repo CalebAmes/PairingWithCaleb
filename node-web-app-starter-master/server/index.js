47569d11bb886440cd87f9752feddad902d71cc2
zero-cool94 <emadmasoud2017@hotmail.com>
2021-01-19 18:48:58 -0600

    finishpase3

--- node-web-app-starter-master/server/index.js
+++ node-web-app-starter-master/server/index.js
@@ -1,14 +1,27 @@
+const { readFile } = require('fs').promises;
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 8081;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('I have items');
+const server = http.createServer(async(req, res) => {
+    if (req.url === "/images") {
+        const imageFilePath = './assets' + req.url;
+        const imageFileContents = await readFile(imageFilePath);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.end(imageFileContents);
+        return;
+    }
+
+
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('I have items');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
