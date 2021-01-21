4a610d8484434fea34b12ea9ace8037af5472698
Caleb <caleb-gilbert@live.com>
2021-01-21 18:35:37 -0500

    phase 2 in prog

--- formative-forms-starter-master/index.js
+++ formative-forms-starter-master/index.js
@@ -1,25 +1,35 @@
-const express = require("express");
+const express = require('express');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "pug");
+const csurfProtection = csurf({ cookie: true });
+
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
 
 const users = [
-  {
-    id: 1,
-    firstName: "Jill",
-    lastName: "Jack",
-    email: "jill.jack@gmail.com"
-  }
+	{
+		id: 1,
+		firstName: 'Jill',
+		lastName: 'Jack',
+		email: 'jill.jack@gmail.com',
+	},
 ];
-app.get("/", (req, res) => {
-  // res.send("Hello World!");
-  // console.log(users[0])
 
-  res.render('index', {users});
+// app.get('/create', csurfProtection, async (req, res) => {
+
+// })
+
+app.get('/', (req, res) => {
+	// res.send("Hello World!");
+	// console.log(users[0])
+
+	res.render('index', { users });
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
