b15069f27a55c60e270fadb26e8c9ef1e0c65d27
Caleb <caleb-gilbert@live.com>
2021-01-22 14:10:23 -0500

    update

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const routes = require("./routes.js");
+const morgan = require("morgan");
+const routes = require("./routes");
 
 
 const app = express();
 
 app.set("view engine", "pug");
 
+app.use(morgan('dev'));
 app.use(routes);
 
+app.use((req, res, next) => {
+  const err = new Error('The requested page couldn\'t be found.');
+  err.status = 404;
+  next(err);
+});
 
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+
+  } else {
+  console.log(err);
+  }
+  next(err);
+})
 
 
 module.exports = app
