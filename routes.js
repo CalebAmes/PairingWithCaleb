644f9143c06ff91807f9c4a43f258ba0f5dc568c
raymondmay95 <raymond.may.95@outlook.com>
2021-01-22 11:58:10 -1000

    added .env file and ignored it in git

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const app = require("./app");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  // throw new Error("This is a test error!");
   res.render("index", { title: "Home" });
 });
 
