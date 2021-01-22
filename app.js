28acb1f08778dccafbe541c3cd2d1191b8f316f7
Caleb <caleb-gilbert@live.com>
2021-01-22 13:43:54 -0500

    on to morgan

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const routes = require("./routes.js");
 
+
 const app = express();
 
 app.set("view engine", "pug");
 
 app.use(routes);
 
-const port = 8080;
-app.listen(port, () => {
-  console.log(`app listening on port ${port}...`);
-});
+
+
+
+module.exports = app
