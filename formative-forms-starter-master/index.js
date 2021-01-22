f87ca420d30a4c6cd45dd1ec1cae7b2183c936ef
Caleb <caleb-gilbert@live.com>
2021-01-22 12:19:58 -0500

    1st part

--- formative-forms-starter-master/index.js
+++ formative-forms-starter-master/index.js
@@ -1,35 +1,22 @@
-const express = require('express');
-const csurf = require('csurf');
-const cookieParser = require('cookie-parser');
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const csurfProtection = csurf({ cookie: true });
+app.set("view engine", "pug");
 
-app.use(cookieParser());
-
-app.set('view engine', 'pug');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const users = [
-	{
-		id: 1,
-		firstName: 'Jill',
-		lastName: 'Jack',
-		email: 'jill.jack@gmail.com',
-	},
+  {
+    id: 1,
+    firstName: "Jill",
+    lastName: "Jack",
+    email: "jill.jack@gmail.com"
+  }
 ];
-
-// app.get('/create', csurfProtection, async (req, res) => {
-
-// })
-
-app.get('/', (req, res) => {
-	// res.send("Hello World!");
-	// console.log(users[0])
-
-	res.render('index', { users });
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
