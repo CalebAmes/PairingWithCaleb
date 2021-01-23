167922f80503033189c1b28e49108a5d4bc7dc8f
Caleb <caleb-gilbert@live.com>
2021-01-22 19:17:47 -0500

    catch up

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const db = require('./db/models');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+    res.render('index', { title: 'Home', books });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
