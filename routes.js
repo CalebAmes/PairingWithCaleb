75cdbcf3d1f6e266d6a3d84e06c1be683e4eff9c
raymondmay95 <raymond.may.95@outlook.com>
2021-01-22 14:05:52 -1000

    finished part 2

--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const app = require("./app");
+
+const db = require("./db/models");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // throw new Error("This is a test error!");
-  res.render("index", { title: "Home" });
+router.get("/", async (req, res, next) => {
+  try {
+    const books = await db.Book.findAll({ order: [["title", "ASC"]] });
+    res.render("index", { title: "Home", books });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
